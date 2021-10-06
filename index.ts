40c1889a9fc0d5ee4b647ce0d851b84f041ab71c
Pignuuu <Pignuuu@gmail.com>
2021-10-06 22:48:31 +0200

    update

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ const program = new Command();
 const puppeteer = require("puppeteer");
 const randomstring = require("randomstring");
 const fs = require("fs");
-const random = require("random");
 
 const noLinkSpecified = () => {
   console.log("Missing argument -l or --link");
@@ -44,7 +43,7 @@ const filename = randomstring.generate({
   length: 10,
   charset: "hex",
 });
-const file = fs.createWriteStream(__dirname + `/videos/${filename}.webm`);
+const file = fs.createWriteStream(__dirname + `/videos/${filename}.mp4`);
 
 async function startRecording() {
   const browser = await launch({
@@ -59,6 +58,16 @@ async function startRecording() {
   await page.goto(options.link);
   await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
   await page.keyboard.press("f");
+  try {
+    await Promise.all([
+      await page.click(
+        `#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-588ddc-0.persistent-player > div > div.Layout-sc-nxg1ff-0.video-player > div > div > div > div > div.Layout-sc-nxg1ff-0.krOuYh.player-overlay-background.player-overlay-background--darkness-0.content-overlay-gate > div > div.Layout-sc-nxg1ff-0.bzQnIQ.content-overlay-gate__allow-pointers > button`
+      ),
+    ]);
+    console.log("Stream is agerestricted");
+  } catch (err) {
+    console.log("Stream is not agerestricted");
+  }
   const stream = await getStream(page, { audio: true, video: true });
   console.log("recording");
 
@@ -67,9 +76,9 @@ async function startRecording() {
     await stream.destroy();
     file.close();
     console.log("finished");
-    
+
     process.exit();
-  }, 15000 * options.time);
+  }, 60000 * options.time);
 }
 
 checkIfUrlIsValid();
