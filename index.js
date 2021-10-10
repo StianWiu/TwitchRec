a63a871b2ab1a1e895b9547c6bdc3560b8b12ea8
Pignuuu <Pignuuu@gmail.com>
2021-10-10 17:32:38 +0200

    Might be done?

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// import { Command } from "commander";
-// const program = new Command();
-// const randomstring = require("randomstring");
-// const fs = require("fs");
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -38,66 +35,228 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-// program.option("-u, --user <username>", "Twitch user to record [Required]");
-// program.parse(process.argv);
-// const options = program.opts();
+exports.__esModule = true;
+var commander_1 = require("commander");
+var program = new commander_1.Command();
+var randomstring = require("randomstring");
+var exec = require("child_process").exec;
 var _a = require("puppeteer-stream"), launch = _a.launch, getStream = _a.getStream;
 var fs = require("fs");
-var exec = require("child_process").exec;
-function test() {
+// Add options for command
+var noUserSpecified = function () {
+    console.log("Missing argument -u or --user");
+    process.exit();
+};
+var noOsSpecified = function () {
+    console.log("Missing argument -w or --windows");
+    process.exit();
+};
+program.option("-u, --user <username>", "Twitch user to record [Required]");
+program.option("-w, --windows <true/false>", "Using windows true or false [Required]");
+program.option("-f, --frames <number>", "How many fps to export to [Optinal]");
+program.option("-o, --output <true/false>", "Print ffmpeg to console? [Optinal]");
+program.parse(process.argv);
+var options = program.opts();
+var windows = undefined;
+var fps = undefined;
+var output = undefined;
+var rerunStream = undefined;
+var checkConfiguration = function () {
+    if (options.user) {
+        if (options.windows == "true" || options.windows == "false") {
+            if (options.windows == "true") {
+                windows = true;
+            }
+            else {
+                windows = false;
+            }
+            if (options.frames) {
+                fps = options.frames;
+            }
+            else {
+                fps = 24;
+            }
+            if (options.output) {
+                if (options.output == "true") {
+                    output = true;
+                }
+                output = false;
+            }
+            else {
+                output = true;
+            }
+        }
+        else
+            noOsSpecified();
+    }
+    else
+        noUserSpecified();
+};
+checkConfiguration();
+var filename = randomstring.generate({
+    length: 10,
+    charset: "hex"
+});
+function startRecording() {
     return __awaiter(this, void 0, void 0, function () {
-        var browser, page, stream, ffmpeg;
+        var browser, page, originalUrl, checkIfLive, checkIfRerun, _a, _b, err_1, stream, ffmpeg;
         var _this = this;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, launch({
-                        // executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe ",
+        return __generator(this, function (_c) {
+            switch (_c.label) {
+                case 0:
+                    browser = undefined;
+                    if (!(windows == true)) return [3 /*break*/, 2];
+                    return [4 /*yield*/, launch({
+                            executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe ",
+                            defaultViewport: {
+                                width: 1920,
+                                height: 1080
+                            }
+                        })];
+                case 1:
+                    browser = _c.sent();
+                    return [3 /*break*/, 4];
+                case 2: return [4 /*yield*/, launch({
                         executablePath: "/usr/bin/google-chrome-stable",
                         defaultViewport: {
-                            width: 1924,
+                            width: 1024,
                             height: 768
                         }
                     })];
-                case 1:
-                    browser = _a.sent();
-                    return [4 /*yield*/, browser.newPage()];
-                case 2:
-                    page = _a.sent();
-                    return [4 /*yield*/, page.goto("https://www.twitch.tv/foxenkin")];
                 case 3:
-                    _a.sent();
-                    return [4 /*yield*/, getStream(page, {
-                            audio: true,
-                            video: true,
-                            frameSize: 1000
-                        })];
-                case 4:
-                    stream = _a.sent();
-                    console.log("recording");
-                    ffmpeg = exec("ffmpeg -async 1 -y -re -i - -r 25 videos/output.mp4");
-                    ffmpeg.stderr.on("data", function (chunk) {
-                        console.log(chunk.toString());
-                    });
-                    stream.pipe(ffmpeg.stdin);
-                    setTimeout(function () { return __awaiter(_this, void 0, void 0, function () {
+                    browser = _c.sent();
+                    _c.label = 4;
+                case 4: return [4 /*yield*/, browser.newPage()];
+                case 5:
+                    page = _c.sent();
+                    return [4 /*yield*/, page.goto("https://www.twitch.tv/" + options.user)];
+                case 6:
+                    _c.sent();
+                    originalUrl = page.url();
+                    checkIfLive = function () { return __awaiter(_this, void 0, void 0, function () {
                         return __generator(this, function (_a) {
                             switch (_a.label) {
-                                case 0: return [4 /*yield*/, stream.destroy()];
+                                case 0: return [4 /*yield*/, page.$("#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.channel-root.channel-root--watch-chat.channel-root--live.channel-root--watch.channel-root--unanimated > div.Layout-sc-nxg1ff-0.bDMqsP.channel-root__main--with-chat > div.channel-root__info.channel-root__info--with-chat > div > div.Layout-sc-nxg1ff-0.jLilpG > div > div > div > div.Layout-sc-nxg1ff-0.iMHulU > div > div > div > a > div.Layout-sc-nxg1ff-0.ScHaloIndicator-sc-1l14b0i-1.dKzslu.tw-halo__indicator > div > div > div")];
                                 case 1:
-                                    _a.sent();
-                                    stream.on("end", function () { });
-                                    ffmpeg.stdin.setEncoding("utf8");
-                                    ffmpeg.stdin.write("q");
-                                    ffmpeg.stdin.end();
-                                    ffmpeg.kill();
-                                    console.log("finished");
+                                    if ((_a.sent()) !== null)
+                                        return [2 /*return*/, true];
+                                    else
+                                        return [2 /*return*/, false];
                                     return [2 /*return*/];
                             }
                         });
-                    }); }, 60000 * 1);
+                    }); };
+                    checkIfRerun = function () { return __awaiter(_this, void 0, void 0, function () {
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, page.$("#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.channel-root.channel-root--watch-chat.channel-root--live.channel-root--watch.channel-root--unanimated > div.Layout-sc-nxg1ff-0.bDMqsP.channel-root__main--with-chat > div.channel-root__info.channel-root__info--with-chat > div > div.Layout-sc-nxg1ff-0.jLilpG > div > div > div > div.Layout-sc-nxg1ff-0.iMexhI > div.Layout-sc-nxg1ff-0.dglwHV > div.Layout-sc-nxg1ff-0.kBOtQI > div > div:nth-child(2) > div > div > div.Layout-sc-nxg1ff-0.ftYIWt > a > span")];
+                                case 1:
+                                    if ((_a.sent()) !== null)
+                                        return [2 /*return*/, true];
+                                    else
+                                        return [2 /*return*/, false];
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); };
+                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 5000); })];
+                case 7:
+                    _c.sent();
+                    _c.label = 8;
+                case 8: return [4 /*yield*/, checkIfLive()];
+                case 9:
+                    if (!((_c.sent()) == false)) return [3 /*break*/, 12];
+                    console.log("Streamer is not live");
+                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 60000); })];
+                case 10:
+                    _c.sent();
+                    return [4 /*yield*/, page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] })];
+                case 11:
+                    _c.sent();
+                    return [3 /*break*/, 8];
+                case 12: return [4 /*yield*/, checkIfRerun()];
+                case 13:
+                    if ((_c.sent()) == false) {
+                        console.log("This stream is a rerun \nContinuing to record anyways");
+                        rerunStream = true;
+                    }
+                    else {
+                        rerunStream = false;
+                    }
+                    return [4 /*yield*/, page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] })];
+                case 14:
+                    _c.sent();
+                    return [4 /*yield*/, page.keyboard.press("f")];
+                case 15:
+                    _c.sent();
+                    _c.label = 16;
+                case 16:
+                    _c.trys.push([16, 19, , 20]);
+                    _b = (_a = Promise).all;
+                    return [4 /*yield*/, page.click("#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-588ddc-0.persistent-player > div > div.Layout-sc-nxg1ff-0.video-player > div > div > div > div > div.Layout-sc-nxg1ff-0.krOuYh.player-overlay-background.player-overlay-background--darkness-0.content-overlay-gate > div > div.Layout-sc-nxg1ff-0.bzQnIQ.content-overlay-gate__allow-pointers > button")];
+                case 17: return [4 /*yield*/, _b.apply(_a, [[
+                            _c.sent()
+                        ]])];
+                case 18:
+                    _c.sent();
+                    console.log('Stream is agerestricted\nClicked "Start Watching" button');
+                    return [3 /*break*/, 20];
+                case 19:
+                    err_1 = _c.sent();
+                    console.log("Stream is not agerestricted");
+                    return [3 /*break*/, 20];
+                case 20: return [4 /*yield*/, getStream(page, {
+                        audio: true,
+                        video: true,
+                        frameSize: 1000
+                    })];
+                case 21:
+                    stream = _c.sent();
+                    ffmpeg = undefined;
+                    if (windows == true) {
+                        ffmpeg = exec("ffmpeg.exe -async 1 -y -i - -r " + fps + " videos/" + options.user + "-" + filename + ".mp4");
+                    }
+                    else {
+                        ffmpeg = exec("ffmpeg -async 1 -y -i - -r " + fps + " videos/" + options.user + "-" + filename + ".mp4");
+                    }
+                    // console logs output from ffmpeg
+                    if (output == true) {
+                        ffmpeg.stderr.on("data", function (chunk) {
+                            console.log(chunk.toString());
+                        });
+                    }
+                    stream.pipe(ffmpeg.stdin);
+                    _c.label = 22;
+                case 22: return [4 /*yield*/, checkIfLive()];
+                case 23:
+                    if (!((_c.sent()) == true)) return [3 /*break*/, 26];
+                    if (originalUrl != page.url()) {
+                        console.log("Stopping recording because streamer raided someone else");
+                        return [3 /*break*/, 26];
+                    }
+                    return [4 /*yield*/, checkIfRerun()];
+                case 24:
+                    if ((_c.sent()) == false && rerunStream == false) {
+                        console.log("Stream is a rerun");
+                        return [3 /*break*/, 26];
+                    }
+                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 15000); })];
+                case 25:
+                    _c.sent();
+                    return [3 /*break*/, 22];
+                case 26: return [4 /*yield*/, stream.destroy()];
+                case 27:
+                    _c.sent();
+                    stream.on("end", function () { });
+                    ffmpeg.stdin.setEncoding("utf8");
+                    ffmpeg.stdin.write("q");
+                    ffmpeg.stdin.end();
+                    ffmpeg.kill();
+                    console.log("finished");
+                    process.exit();
                     return [2 /*return*/];
             }
         });
     });
 }
-test();
+startRecording();
--- index.ts
+++ index.ts
@@ -1,54 +1,186 @@
-// import { Command } from "commander";
-// const program = new Command();
-// const randomstring = require("randomstring");
-// const fs = require("fs");
-
-// program.option("-u, --user <username>", "Twitch user to record [Required]");
-// program.parse(process.argv);
-// const options = program.opts();
-
+import { Command } from "commander";
+const program = new Command();
+const randomstring = require("randomstring");
+const { exec } = require("child_process");
 const { launch, getStream } = require("puppeteer-stream");
 const fs = require("fs");
-const { exec } = require("child_process");
 
-async function test() {
-  const browser = await launch({
-    // executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe ",
-    executablePath: "/usr/bin/google-chrome-stable",
-    defaultViewport: {
-      width: 1924,
-      height: 768,
-      // width: 1920,
-      // height: 1080,
-    },
-  });
+// Add options for command
+const noUserSpecified = () => {
+  console.log("Missing argument -u or --user");
+  process.exit();
+};
+const noOsSpecified = () => {
+  console.log("Missing argument -w or --windows");
+  process.exit();
+};
+
+program.option("-u, --user <username>", "Twitch user to record [Required]");
+program.option(
+  "-w, --windows <true/false>",
+  "Using windows true or false [Required]"
+);
+program.option("-f, --frames <number>", "How many fps to export to [Optinal]");
+program.option(
+  "-o, --output <true/false>",
+  "Print ffmpeg to console? [Optinal]"
+);
+
+program.parse(process.argv);
+const options = program.opts();
+
+let windows = undefined;
+let fps = undefined;
+let output = undefined;
+let rerunStream = undefined;
+
+const checkConfiguration = () => {
+  if (options.user) {
+    if (options.windows == "true" || options.windows == "false") {
+      if (options.windows == "true") {
+        windows = true;
+      } else {
+        windows = false;
+      }
+      if (options.frames) {
+        fps = options.frames;
+      } else {
+        fps = 24;
+      }
+      if (options.output) {
+        if (options.output == "true") {
+          output = true;
+        }
+        output = false;
+      } else {
+        output = true;
+      }
+    } else noOsSpecified();
+  } else noUserSpecified();
+};
+checkConfiguration();
+
+const filename = randomstring.generate({
+  length: 10,
+  charset: "hex",
+});
+
+async function startRecording() {
+  let browser = undefined;
+  if (windows == true) {
+    browser = await launch({
+      executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe ",
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+    });
+  } else {
+    browser = await launch({
+      executablePath: "/usr/bin/google-chrome-stable",
+      defaultViewport: {
+        width: 1024,
+        height: 768,
+      },
+    });
+  }
 
   const page = await browser.newPage();
-  await page.goto("https://www.twitch.tv/foxenkin");
+  await page.goto(`https://www.twitch.tv/${options.user}`);
+  const originalUrl = page.url();
+
+  const checkIfLive = async () => {
+    if (
+      (await page.$(
+        `#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.channel-root.channel-root--watch-chat.channel-root--live.channel-root--watch.channel-root--unanimated > div.Layout-sc-nxg1ff-0.bDMqsP.channel-root__main--with-chat > div.channel-root__info.channel-root__info--with-chat > div > div.Layout-sc-nxg1ff-0.jLilpG > div > div > div > div.Layout-sc-nxg1ff-0.iMHulU > div > div > div > a > div.Layout-sc-nxg1ff-0.ScHaloIndicator-sc-1l14b0i-1.dKzslu.tw-halo__indicator > div > div > div`
+      )) !== null
+    )
+      return true;
+    else return false;
+  };
+  const checkIfRerun = async () => {
+    if (
+      (await page.$(
+        `#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.channel-root.channel-root--watch-chat.channel-root--live.channel-root--watch.channel-root--unanimated > div.Layout-sc-nxg1ff-0.bDMqsP.channel-root__main--with-chat > div.channel-root__info.channel-root__info--with-chat > div > div.Layout-sc-nxg1ff-0.jLilpG > div > div > div > div.Layout-sc-nxg1ff-0.iMexhI > div.Layout-sc-nxg1ff-0.dglwHV > div.Layout-sc-nxg1ff-0.kBOtQI > div > div:nth-child(2) > div > div > div.Layout-sc-nxg1ff-0.ftYIWt > a > span`
+      )) !== null
+    )
+      return true;
+    else return false;
+  };
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  while ((await checkIfLive()) == false) {
+    console.log("Streamer is not live");
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+  }
+  if ((await checkIfRerun()) == false) {
+    console.log("This stream is a rerun \nContinuing to record anyways");
+    rerunStream = true;
+  } else {
+    rerunStream = false;
+  }
+
+  await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+  await page.keyboard.press("f");
+  try {
+    await Promise.all([
+      await page.click(
+        `#root > div > div.Layout-sc-nxg1ff-0.ldZtqr > div.Layout-sc-nxg1ff-0.iLYUfX > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-588ddc-0.persistent-player > div > div.Layout-sc-nxg1ff-0.video-player > div > div > div > div > div.Layout-sc-nxg1ff-0.krOuYh.player-overlay-background.player-overlay-background--darkness-0.content-overlay-gate > div > div.Layout-sc-nxg1ff-0.bzQnIQ.content-overlay-gate__allow-pointers > button`
+      ),
+    ]);
+    console.log('Stream is agerestricted\nClicked "Start Watching" button');
+  } catch (err) {
+    console.log("Stream is not agerestricted");
+  }
+
   const stream = await getStream(page, {
     audio: true,
     video: true,
     frameSize: 1000,
   });
-  console.log("recording");
   // this will pipe the stream to ffmpeg and convert the webm to mp4 format
-  const ffmpeg = exec(`ffmpeg -async 1 -y -re -i - -r 25 videos/output.mp4`);
-  ffmpeg.stderr.on("data", (chunk) => {
-    console.log(chunk.toString());
-  });
+  let ffmpeg = undefined;
+  if (windows == true) {
+    ffmpeg = exec(
+      `ffmpeg.exe -async 1 -y -i - -r ${fps} videos/${options.user}-${filename}.mp4`
+    );
+  } else {
+    ffmpeg = exec(
+      `ffmpeg -async 1 -y -i - -r ${fps} videos/${options.user}-${filename}.mp4`
+    );
+  }
+  // console logs output from ffmpeg
+  if (output == true) {
+    ffmpeg.stderr.on("data", (chunk) => {
+      console.log(chunk.toString());
+    });
+  }
 
   stream.pipe(ffmpeg.stdin);
 
-  setTimeout(async () => {
-    await stream.destroy();
-    stream.on("end", () => {});
-    ffmpeg.stdin.setEncoding("utf8");
-    ffmpeg.stdin.write("q");
-    ffmpeg.stdin.end();
-    ffmpeg.kill();
+  while ((await checkIfLive()) == true) {
+    if (originalUrl != page.url()) {
+      console.log("Stopping recording because streamer raided someone else");
+      break;
+    }
+    if ((await checkIfRerun()) == false && rerunStream == false) {
+      console.log("Stream is a rerun");
+      break;
+    }
+    await new Promise((resolve) => setTimeout(resolve, 15000));
+  }
 
-    console.log("finished");
-  }, 60000 * 1);
+  await stream.destroy();
+  stream.on("end", () => {});
+  ffmpeg.stdin.setEncoding("utf8");
+  ffmpeg.stdin.write("q");
+  ffmpeg.stdin.end();
+  ffmpeg.kill();
+
+  console.log("finished");
+  process.exit();
 }
 
-test();
+startRecording();
